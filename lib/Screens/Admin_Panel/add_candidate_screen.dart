602dd6fb2d825781fac00c632cd34f3b84194c3f
ak375456 <ak375456@gmail.com>
2024-10-27 19:57:28 +0500

    admin Screen

--- lib/Screens/Admin_Panel/add_candidate_screen.dart
+++ lib/Screens/Admin_Panel/add_candidate_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:vote_tracker/Screens/Admin_Panel/register_candidate.dart';
 import 'package:vote_tracker/reusable_widgets/my_form.dart';
+import 'package:vote_tracker/services/candidate_services/candidate_services.dart';
 
 class AddCandidateScreen extends StatefulWidget {
   const AddCandidateScreen({super.key});
@@ -11,6 +17,12 @@ class AddCandidateScreen extends StatefulWidget {
 
 class _AddCandidateScreenState extends State<AddCandidateScreen> {
   TextEditingController _controller = TextEditingController();
+  final CandidateServices _candidateService = CandidateServices();
+
+  Future<List<Map<String, dynamic>>> _fetchAllCandidates() {
+    return _candidateService.fetchAllCandidates();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +33,7 @@ class _AddCandidateScreenState extends State<AddCandidateScreen> {
         padding: REdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
-            SizedBox(
-              height: 12.h,
-            ),
+            SizedBox(height: 12.h),
             Row(
               children: [
                 Flexible(
@@ -39,7 +49,14 @@ class _AddCandidateScreenState extends State<AddCandidateScreen> {
                 IconButton.outlined(
                   tooltip: "Add Candidate",
                   color: Colors.blue,
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterCandidate(),
+                      ),
+                    );
+                  },
                   icon: const Icon(
                     Icons.add,
                     color: Colors.blue,
@@ -47,9 +64,179 @@ class _AddCandidateScreenState extends State<AddCandidateScreen> {
                 ),
               ],
             ),
+            SizedBox(height: 20.h),
+            Expanded(
+              child: FutureBuilder(
+                future: _fetchAllCandidates(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("No candidates found."));
+                  }
+
+                  final candidates = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: candidates.length,
+                    itemBuilder: (context, index) {
+                      final candidate = candidates[index];
+                      final partyFlagPath =
+                          partyFlags[candidate['party']] ?? '';
+                      // log(candidate[]);
+                      return Slidable(
+                        direction: Axis.horizontal,
+                        endActionPane: ActionPane(
+                          motion: const StretchMotion(),
+                          children: [
+                            SlidableAction(
+                              onPressed: (BuildContext context) {
+                                log(candidate['uid']);
+                                _candidateService.deleteCandidateAccount(
+                                  context,
+                                  uid: candidate['uid'],
+                                );
+                              },
+                              icon: FontAwesomeIcons.deleteLeft,
+                              backgroundColor: Colors.redAccent,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15.r)),
+                            ),
+                          ],
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              width: 1.w,
+                              color: const Color.fromARGB(120, 0, 0, 0),
+                            ),
+                          ),
+                          width: MediaQuery.of(context).size.width,
+                          height: 100,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: REdgeInsets.only(left: 8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      // mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          extractTextInBrackets(
+                                            candidate['party'],
+                                          ),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            CircleAvatar(
+                                              maxRadius: 25,
+                                              backgroundImage: NetworkImage(
+                                                candidate['image'],
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              width: 5.h,
+                                            ),
+                                            Column(
+                                              // crossAxisAlignment:
+                                              // CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  candidate['fullName'],
+                                                  style: TextStyle(
+                                                      fontSize: 14.sp,
+                                                      fontWeight:
+                                                          FontWeight.w400),
+                                                ),
+                                                Text(
+                                                  candidate['fullAddress']
+                                                      .toString()
+                                                      .substring(
+                                                          0,
+                                                          candidate['fullAddress']
+                                                                  .toString()
+                                                                  .length -
+                                                              9),
+                                                  style: TextStyle(
+                                                      fontSize: 12.sp,
+                                                      color: Color(0xff585858)),
+                                                ),
+                                              ],
+                                            ),
+                                            Container(
+                                              height: 40,
+                                              width: 40,
+                                              decoration: BoxDecoration(
+                                                border: Border.all(width: 1),
+                                                borderRadius:
+                                                    BorderRadius.circular(90),
+                                              ),
+                                              child: const Center(
+                                                child: Text(
+                                                  "MNA",
+                                                  style: TextStyle(
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    Expanded(
+                                      child: Align(
+                                        alignment: Alignment
+                                            .centerRight, // Aligns the image to the right
+                                        child: Container(
+                                          height: 80
+                                              .h, // Set a consistent height for the container
+                                          child: Image.asset(
+                                            partyFlagPath,
+                                            fit: BoxFit
+                                                .contain, // Keeps image proportions consistent within height
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+const Map<String, String> partyFlags = {
+  'Pakistan Muslim League-Nawaz (PML-N)': 'assets/partyflags/PML-N.png',
+  'Pakistan Tehreek-e-Insaf (PTI)': 'assets/partyflags/PTI.png',
+  "Pakistan Peoples Party (PPP)": 'assets/partyflags/PPPP.png',
+  'Jamiat Ulema-e-Islam (F)': 'assets/partyflags/JUI.png',
+  'Tehreek-e-Labbaik Pakistan (TLP)': 'assets/partyflags/TLP.png'
+  // Add more party mappings as necessary
+};
+String extractTextInBrackets(String text) {
+  final RegExp regex = RegExp(r'\(([^)]+)\)');
+  final match = regex.firstMatch(text);
+  return match != null ? match.group(1) ?? '' : '';
 }
